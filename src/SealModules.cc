e4e7842643ac7aeb9c507e7f2598bd522c42f9bf
cmsbuild <>
2006-03-27 12:57:18 +0000

    This commit was manufactured by cvs2git to create tag 'RecoLocalTracker- SiPixelRecHits-V00-00-11'.
    Sprout from master 2006-03-27 12:57:16 UTC Vincenzo Chiochia <sha1-62818cb5780e9873c1b72b7686c98ffb95579570@cern.ch> 'std::cout messages moved to MessageLogger'
    Delete:
        BuildFile
        doc/html/index.html
        doc/html/overview.html
        interface/CPEFromDetPosition.h
        interface/EtaCorrection.h
        interface/SiPixelRecHitConverter.h
        src/CPEFromDetPosition.cc
        src/EtaCorrection.cc
        src/SealModules.cc
        src/SiPixelRecHitConverter.cc

--- src/SealModules.cc
+++
@@ -1,8 +0,0 @@
-#include "PluginManager/ModuleDef.h"
-#include "FWCore/Framework/interface/MakerMacros.h"
-
-#include "RecoLocalTracker/SiPixelRecHits/interface/SiPixelRecHitConverter.h"
-
-using cms::SiPixelRecHitConverter;
-DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_MODULE(SiPixelRecHitConverter)
