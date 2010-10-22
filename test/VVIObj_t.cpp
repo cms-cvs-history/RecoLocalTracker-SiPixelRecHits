249df2138d4316e988383400d140b74287672edf
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2010-10-22 07:43:37 +0000

    fix it

--- test/VVIObj_t.cpp
+++ test/VVIObj_t.cpp
@@ -22,14 +22,14 @@ int main() {
     res = dzero(5,15,x0,rv,1.e-5,1000,afun);
     std::cout << res << " " << x0 << " " << rv << " " << std::endl;
 
-    double kappa[] = { 0.0681354, 0.0725822, 2.27383  };
-    double xvav[]  = { 0.434149,  7.45804, 2.27383  };
+    double kappa[] = { 0.0681354, 0.0725822, 0.0658784};
+    double xvav[]  = { 0.434149,  7.45804,  -0.397674  };
 
     double beta2=1;
     for (int i=0; i!=3; ++i) {
       VVIObj vvidist(kappa[i], beta2, 1);
       double prvav = vvidist.fcn(xvav[i]);
-      std::cout << "vav: " << kappa << " " << xvav << " " << prvav
+      std::cout << "vav: " << kappa[i] << " " << xvav[i] << " " << prvav
                << " " << TMath::VavilovI(xvav[i], kappa[i], beta2) << std::endl;
     }
 
