0821813d1d4074f901f9fbbf629b59eb26ab9464
Vincenzo Innocente <sha1-48872ea38f0e6361a52cfb8d57ac997c453089e2@cern.ch>
2010-10-22 07:08:11 +0000

    comppiles

--- test/VVIObj_t.cpp
+++ test/VVIObj_t.cpp
@@ -1,23 +1,25 @@
-#include "RecoLocalTracker/SiPixelRecHits/interface/VVIObj.cc"
+#include "RecoLocalTracker/SiPixelRecHits/src/VVIObj.cc"
 #include<iostream>
 
-double afun(x) { return x*x;}
+double afun(double x) { return x*x;}
 
 int main() {
+  using namespace VVIObjDetails;
+
   double  sint=0;
   double  cint=0;
-  for (double x=-20.5;x<20.5; x++2) {
+  for (double x=-20.5;x<20.5; x+=2) {
     sincosint(x, sint, cint);
     std::cout << sint << " " << cint << " " 
 	      << sinint(x) << " " << cosint(x) << std::endl;
-
-    double x0, double rv;
+  }
+    double x0; double rv;
     int res = dzero(-5,5,x0,rv,1.e-5,1000,afun);
-    std::cout << res " " << x0 << " " << rv << " " std::endl;
-    int res = dzero(5,-5,x0,rv,1.e-5,1000,afun);
-    std::cout << res " " << x0 << " " << rv << " " std::endl;
-    int res = dzero(5,15,x0,rv,1.e-5,1000,afun);
-    std::cout << res " " << x0 << " " << rv << " " std::endl;
+    std::cout << res << " " << x0 << " " << rv << " " << std::endl;
+    res = dzero(5,-5,x0,rv,1.e-5,1000,afun);
+    std::cout << res << " " << x0 << " " << rv << " " << std::endl;
+    res = dzero(5,15,x0,rv,1.e-5,1000,afun);
+    std::cout << res << " " << x0 << " " << rv << " " << std::endl;
 
    return 0;
 
